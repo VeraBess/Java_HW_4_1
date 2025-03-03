c624c336db3a66bdb9da453e12e1eca88616a193
vera bessonova <v2112365@gmail.com>
2025-03-03 11:16:14 +0300

    added code to Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,9 @@ public class Main {
 
     public static void main(String[] args) {
         BonusService service = new BonusService();
-        long bonus = service.calculate(5_000, true);
-        System.out.println(bonus);
+
+        int expected = 150; //ожидаемый результат
+        long actual = service.calculate(5_000, true); // фактический результат
+        System.out.println(expected + "==?==" + actual);
     }
 }
